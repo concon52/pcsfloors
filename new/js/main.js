0a9892789f77712e8da6c0b150e8b73a197657ab
LoganBesecker <lbesecker195@gmail.com>
2016-07-15 18:13:00 -0700

    updated catalog page: clicking thumbnail opens product popover; hover on thumbnail zooms in; updated header

--- new/js/main.js
+++ new/js/main.js
@@ -28,8 +28,55 @@ jQuery(function($) {'use strict',
 	});
 	
 	//Pretty Photo
-	 $("a[data-gallery^='prettyPhoto']").prettyPhoto({
-	  social_tools: false
+//	 $("a[data-gallery^='prettyPhoto']").prettyPhoto({
+	  $("a.portfolioAnchor").prettyPhoto({	
+	  social_tools: false,
+	  deeplinking: true,
+	  markup: '<div class="pp_pic_holder"> \
+						<div class="ppt">&nbsp;</div> \
+						<div class="pp_top"> \
+							<div class="pp_left">  \
+							</div> \
+							<div class="pp_middle"> \
+							</div> \
+							<div class="pp_right"> \
+											<a class="pp_close productCloseButton" href="#">Close</a> \
+							</div> \
+						</div> \
+						<div class="pp_content_container"> \
+							<div class="pp_left"> \
+							<div class="pp_right"> \
+								<div class="pp_content"> \
+									<div class="pp_loaderIcon"></div> \
+									<div class="pp_fade"> \
+										<a href="#" class="pp_expand" title="Expand the image">Expand</a> \
+										<div class="pp_hoverContainer"> \
+											<a class="pp_next" href="#">next</a> \
+											<a class="pp_previous" href="#">previous</a> \
+										</div> \
+										<div id="pp_full_res"></div> \
+										<div class="pp_details"> \
+											<div class="pp_nav"> \
+												<a href="#" class="pp_arrow_previous">Previous</a> \
+												<p class="currentTextHolder">0/0</p> \
+												<a href="#" class="pp_arrow_next">Next</a> \
+											</div> \
+											<p class="pp_description"></p> \
+											{pp_social} \
+											<button class="btn addToCartButton">Add Sample To Cart</button> \
+										</div> \
+									</div> \
+								</div> \
+							</div> \
+							</div> \
+						</div> \
+						<div class="pp_bottom"> \
+							<div class="pp_left"></div> \
+							<div class="pp_middle"> </div> \
+							<div class="pp_right"></div> \
+						</div> \
+					</div> \
+					<div class="pp_overlay"></div>'
 	 });
 
 
@@ -82,13 +129,13 @@ function Scroll() {
 	var winTop      =   $(window).scrollTop();
 	var rangeTop    =   200;
 	var rangeBottom =   500;
-	$('.navbar-collapse').find('.scroll a').each(function(){
+	$('.navbar-collapse').find('.scroll > a').each(function(){
 		contentTop.push( $( $(this).attr('href') ).offset().top);
 		contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
 	})
 	$.each( contentTop, function(i){
 		if ( winTop > contentTop[i] - rangeTop ){
-			$('.navbar-collapse li.scroll')
+			$('.navbar-collapse > ul > li.scroll')
 			.removeClass('active')
 			.eq(i).addClass('active');			
 		}
