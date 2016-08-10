bd88c2ff2288bb2cf5d2b5f399ceccca9f13cb3b
LoganBesecker <lbesecker195@gmail.com>
2016-08-10 14:37:17 -0700

    scraperHelpers.py now has completed Capri @ Home and Fritztile scraping functionality.  .gitignore also now ignores cached compiled python scripts, so only the source scripts are stored in git.

--- new/scrapers/scraperHelpers.py
+++ new/scrapers/scraperHelpers.py
@@ -1,32 +1,112 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin
+from unidecode import unidecode
 import requests
 import json
 
 
 
+
+def scrapeEverything(insertProducts = False, printProducts = True):
+	# scrape all Capri @ Home products
+	# url: http://www.capriathome.com/products.htm
+	ca = capriAtHome()
+	ca.scrapeProducts(insertProducts = insertProducts, printProducts = printProducts)
+
+	# scrape all tiles from 'Tile Gallery' on Fritztile
+	# url: http://www.fritztile.com/fritz-products/tile-gallery/classic-terrazzo-collection/c520515/
+	f = fritzTile()
+	f.scrapeProducts(insertProducts = insertProducts, printProducts = printProducts)
+
+	# scrape all custom tiles from 'Tile Gallery' on fritztile
+	# url: http://www.fritztile.com/fritz-products/custom-tile-gallery/
+	f = fritzTile()
+	f.scrapeProducts(insertProducts = insertProducts, printProducts = printProducts)
+
+
+
+
+
 class baseDomainSpecificScraperClass:
-	def __init__(self, url = "http://www.capriathome.com/cork_glue_down_tiles_and_planks.htm"):
+	def __init__(self, urls = ["http://www.capriathome.com/products.htm"]):
+		# determine if we were provided a single string or an array of strings
+		if isinstance(urls, str):
+			self.productURLs = []
+			self.url = urls
+		elif isinstance(urls, list) and len(urls) > 0 and isinstance(urls[0], str):
+			self.productURLs = urls
+			self.url = self.productURLs.pop()
+		else:
+			raise Exception("Error, provided incorrect type for `urls` variable")
+
+		self.r = requests.get(self.url)
+		self.url = self.r.url
+		self.soup = bs(self.r.text)
+
+
+	def changeURL(self, url):
 		self.url = url
 		self.html = requests.get(self.url).text
-		self.soup = BeautifulSoup(self.html)
+		self.soup = bs(self.html)
 
 
-	def getProductInfo(self):
+	def insertProductIntoDB(self):
+		requests.post("http://s497483439.onlinehome.us/queryTemplate.php", self.getProductInfo(jsonEncode = True))
+
+
+	def getProductInfo(self, jsonEncode = False, indent = None):
 		#get all the things
-		return json.dumps({
-		'title' : self.getTitle(),
-		'description' : self.getDescription(),
-		'productPictureURLs' : self.getProductPictureURLs(),
-		'colors' : self.getColors(),
-		'manufacturer' : self.getManufacturer(),
-		'flooringType' : self.getFlooringType(),
-		'additionalInfo' : self.getAdditionalData()
-		}, sort_keys = True, indent = 4)
+		r = {
+			'name' : formatStr(self.getTitle()),
+			'description' : formatStr(self.getDescription()),
+			'picture' : self.getProductPictureURLs(),
+			'colors' : self.getColors(),
+			'manufacturer' : formatStr(self.getManufacturer()),
+			'type' : formatStr(self.getFlooringType()),
+			'additionalInfo' : self.getAdditionalData(),
+			'manurl' : self.getManURL()
+		}
+
+		if jsonEncode:
+			return json.dumps(r, sort_keys = True, indent = indent)
+
+		return r
+
+
+	# scrapes all products in self.productURLs
+	# if a newURLs array is provided, it will also scrape those URLs
+	def scrapeProducts(self, newURLs = [], searchForMoreProducts = True, insertProducts = False, printProducts = True):
+		# make sure newURLs is either empty or an array of strings, otherwise set it to an empty array so it doesn't cause errors
+		if not ( isinstance(newURLs, list) and ( len(newURLs) == 0 or isinstance(newURLs[0], str) ) ):
+			newURLs = []
+
+
+		# should we search the current URL for more product pages?
+		if searchForMoreProducts:
+			self.productURLs += self.getProductURLs()
+
+		# append the newURLs to self.productURLs and only keep unique URLs
+		self.productURLs = list(set(newURLs + self.productURLs)) 
+
+		# scrape all the products
+		for productURL in self.productURLs:
+			self.changeURL(productURL)
+
+			if insertProducts:
+				self.insertProductIntoDB()
+
+			if printProducts:
+				self.printProductInfo()
+				print("\n\n\n")
+
+
+	# manufacturer URL
+	def getManURL(self):
+		return self.url.split('/')[2]
 
 
 	def printProductInfo(self):
-		print(self.getProductInfo())
+		print(self.getProductInfo(jsonEncode = True, indent = 4))
 
 
 	# Convert the scraped url to absolute form
@@ -34,15 +114,22 @@ class baseDomainSpecificScraperClass:
 		return urljoin(self.url, urlToConvert)
 
 
+	def formatStr(self, s):
+		return unidecode(s).strip()
+
+
+
+	def getProductURLs(self):
+		print("getProdutURLs() stub")
+		return []
+
 	def getTitle(self):
 		print("getTitle() stub")
 		return ""
 
-
 	def getDescription(self):
 		print("getDescriontion() stub")
 		return ""
-
 
 	def getProductPictureURLs(self):
 		print("getProductPicureURLs() stub")
@@ -67,24 +154,263 @@ class baseDomainSpecificScraperClass:
 
 
 
+
+############################################################################################
+##################################### van Gelder ###########################################
+############################################################################################
+
+class vanGelder(baseDomainSpecificScraperClass):
+	def __init__(self, urls = ["http://www.vangelder-inc.com/products/logo-mats/ovation-36/"]):
+		super().__init__(urls)
+
+	def getProductURLs(self):
+		productURLs = []
+		anchors = self.soup.find(id = 'tileBar').findAll('a')
+		
+		for anchor in anchors:
+			url = anchor.get("href")
+			url = self.urlToAbsolute(url)
+			productURLs.append(url)
+
+		return productURLs
+
+
+	def getTitle(self):
+		t = self.soup.find(class_ = 'p-title').p.text
+		return self.formatStr(t)
+
+	def getDescription(self):
+		# create local soup since we are going to remove a portion of it
+		soup = self.soup
+
+		# remove all text that can cause issues
+		for s in soup.findAll(id = 'column-clear'):
+			s.clear()
+
+		d = soup.find(class_ = 'p-body').text
+		return self.formatStr(d)
+
+
+	def getProductPictureURLs(self):
+		productURLs = []
+		imgs = self.soup.find(class_ = 'p-header2').findAll('img')
+
+		for img in imgs:
+			url = img.get('src')
+			url = self.urlToAbsolute(url)
+			productURLs.append(url)
+
+		return productURLs
+
+
+	def getColors(self):
+		imageAreas = self.soup.findAll(class_ = 'imageareaContent')
+		colors = []
+
+		for imageArea in imageAreas:
+			for i, caption in enumerate(imageArea.findAll(class_ = 'caption_container')):
+				# skip the first image if there are multiple imageAreas because it is used to show the colors of that group
+				if len(imageAreas) is 1 and i is 0: 
+					continue
+
+				colors.append({
+					'url' : self.urlToAbsolute(caption.img.get('src')),
+					'name' : caption.text,
+					'css' : ''	
+				})
+
+		return colors
+
+
+	def getManufacturer(self):
+		return "Fritztile"
+
+
+	def getFlooringType(self):
+		return "tile"
+
+	def getAdditionalData(self):
+		pdfs = self.soup.find(class_ = 'orange').findAll('a')
+		data = {'pdfs' : []}
+
+		for pdf in pdfs:
+			data['pdfs'].append({
+				'url' : self.urlToAbsolute(pdf.get('href')),
+				'text' : pdf.text
+			})
+
+		return data
+
+
+
+
+############################################################################################
+##################################### Fritztile ############################################
+############################################################################################
+
+class fritzTile(baseDomainSpecificScraperClass):
+	def __init__(self, urls = ["http://www.fritztile.com/fritz-products/tile-gallery/classic-terrazzo-collection/c520515/"]):
+		super().__init__(urls)
+
+	def getProductURLs(self):
+		productURLs = []
+		anchors = self.soup.find(id = 'tileBar').findAll('a')
+		
+		for anchor in anchors:
+			url = anchor.get("href")
+			url = self.urlToAbsolute(url)
+			productURLs.append(url)
+
+		return productURLs
+
+
+	def getTitle(self):
+		return self.soup.find(id = "tileBar").find(class_ = "selected").text
+
+	def getDescription(self):
+		return self.soup.find(id = "contentarea").findAll('div')[1].div.text
+
+	def getProductPictureURLs(self):
+		return []
+
+	def getColors(self):
+		thumbs = self.soup.findAll(class_ = 'slide')
+		colors = []
+
+		for thumb in thumbs:
+			colors.append({
+				'url' : self.urlToAbsolute(thumb.img.get('src')),
+				'name' : thumb.text.strip().replace('\n', ' '),
+				'css' : ''
+			})
+
+		return colors
+
+	def getManufacturer(self):
+		return "Fritztile"
+
+
+	def getFlooringType(self):
+		return "tile"
+
+	def getAdditionalData(self):
+		pdfs = self.soup.find(class_ = 'orange').findAll('a')
+		data = {'pdfs' : []}
+
+		for pdf in pdfs:
+			data['pdfs'].append({
+				'url' : self.urlToAbsolute(pdf.get('href')),
+				'text' : pdf.text
+			})
+
+		return data
+
+
+
+
+############################################################################################
+################################ Fritztile Custom Tile #####################################
+############################################################################################
+
+class fritzTileCustomTile(fritzTile):
+	def __init__(self, urls = ["http://www.fritztile.com/fritz-products/custom-tile-gallery/"]):
+		super().__init__(urls)
+
+
+	# this is a gutted version of other scrapeProducts() because Fritztile's Custom Tiles are basically one product with a lot of colors
+	def scrapeProducts(self, newURLs = [], searchForMoreProducts = False, insertProducts = True, printProducts = False):
+		if insertProducts:
+			self.insertProductIntoDB()
+
+		if printProducts:
+			self.printProductInfo()
+			print("\n\n\n")
+
+
+	def getProductURLs(self):
+		productURLs = []
+		ids = self.soup.find(id = 'tileBar').findAll('a')
+		
+		for anchor in anchors:
+			url = anchor.get("href")
+			url = self.urlToAbsolute(url)
+			productURLs.append(url)
+
+		return productURLs
+
+
+	def getTitle(self):
+		return "Custom Tile"
+
+	def getDescription(self):
+		return list(self.soup.find(id = "contentarea").findAll('div')[1].div.stripped_strings)[1]
+
+
+	def getColors(self):
+		def ajaxColors(ajaxID):
+			r = requests.get("http://www.fritztile.com/custom-tile-list/?id=" + ajaxID)
+			c = []
+
+			for soup in bs(r.text).findAll('div'):
+				c.append({
+					'url' : self.urlToAbsolute(soup.a.get('href')),
+					'name' : soup.a.get('title'),
+					'css' : ''
+				})
+
+			print('ajaxID: ' + ajaxID, c, 'n: ' + str(len(c)), '\n\n\n')
+
+			return c
+
+		colors = []
+		thumbs = self.soup.find(id = 'hue-bar').findAll('li')
+
+		for thumb in thumbs:
+			ajaxID = thumb.a.get('onclick').split("'")[1]
+
+			colors += ajaxColors(ajaxID)	
+
+		return colors
+
+
+
+	def getAdditionalData(self):
+		return []
+
+
+
+
+
 ############################################################################################
 ##################################### Capri At Home ########################################
 ############################################################################################
 
 class capriAtHome(baseDomainSpecificScraperClass):
-	def __init__(self, url = "http://www.capriathome.com/cork_glue_down_tiles_and_planks.htm"):
-		super().__init__(url)
+	def __init__(self, urls = ["http://www.capriathome.com/products.htm"]):
+		super().__init__(urls)
+
+
+	def getProductURLs(self):
+		productURLs = []
+		mainThumbs = self.soup.findAll(id = "mainthumbs")
+		
+		for thumb in mainThumbs:
+			url = thumb.a.get("href")
+			url = self.urlToAbsolute(url)
+			productURLs.append(url)
+
+		return productURLs
 
 
 	def getTitle(self):
-		return self.soup.find(id="title").get_text()
+		return self.soup.find(id="title").text
 
 
 	def getDescription(self):
 		info = self.soup.find(class_ = "information") # locate the main description section
 		[s.extract() for s in info(['a', 'span'])] # remove span and anchor elements
-		info = info.get_text() 
-		info = info.strip()
+		info = info.text 
+		info = info.strip() # strip white space at start and end of string
 		info = info.replace('\n', '</br>') 
 		return info
 
@@ -106,7 +432,7 @@ class capriAtHome(baseDomainSpecificScraperClass):
 		for thumb in thumbs:
 			colors.append({
 				'url' : self.urlToAbsolute(thumb.a.get('href')),
-				'name' : thumb.get_text(),
+				'name' : thumb.text,
 				'css' : ''
 			})
 
@@ -132,12 +458,12 @@ class capriAtHome(baseDomainSpecificScraperClass):
 
 	def getAdditionalData(self):
 		pdfs = self.soup.findAll(class_ = 'pdf')
-		data = []
+		data = {'pdfs' : []}
 
 		for pdf in pdfs:
-			data.append({
+			data['pdfs'].append({
 				'url' : self.urlToAbsolute(pdf.get('href')),
-				'text' : pdf.get_text()
+				'text' : pdf.text
 			})
 
 		return data
