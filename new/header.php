a93c40d808b2bc0cd0d1703359d02bfc95739d46
concon52 <conorsullivan@nevada.unr.edu>
2016-09-21 23:12:11 -0700

    added google anal

--- new/header.php
+++ new/header.php
@@ -38,6 +38,18 @@ echo '
 		<link rel="apple-touch-icon-precomposed" href="images/ico/pcs_logo.png">
 
 		<script type="text/javascript" src="js/jquery.js"></script> 
+		
+		<script>
+		  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+
+		  ga("create", "UA-82319911-2", "auto");
+		  ga("send", "pageview");
+
+		</script>
+			
 	</head><!--/head-->
 	<body>
 		<header id="navigation"> 
